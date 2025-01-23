3d83f67747b7f2efd5b52a59454c2620600914e3
Aman Belwal <amanbelwal11@gmail.com>
2025-01-23 23:41:48 +0530

    setup and auth

---
+++ CodeSync/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-10">
+       
+    </div>  
+  );
+}

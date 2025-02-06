7fe44c44dcc84778614479f474796b6c4e98b9e1
Aman Belwal <amanbelwal11@gmail.com>
2025-02-06 12:11:05 +0530

    added api's(interview,user,comment)

--- CodeSync/convex/_generated/api.d.ts
+++ CodeSync/convex/_generated/api.d.ts
@@ -13,7 +13,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as comments from "../comments.js";
 import type * as http from "../http.js";
+import type * as Interview from "../Interview.js";
 import type * as users from "../users.js";
 
 /**
@@ -25,7 +27,9 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  comments: typeof comments;
   http: typeof http;
+  Interview: typeof Interview;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
--- CodeSync/convex/schema.ts
+++ CodeSync/convex/schema.ts
@@ -8,7 +8,25 @@ export default defineSchema({
         name: v.string(),
         email:v.string(),
         image:v.optional(v.string()),
-        role:v.union(v.literal("Candidate"),v.literal("Interviewer")),
+        role:v.union(v.literal("candidate"),v.literal("interviewer")),
         clerkId : v.string(),
-    }).index("byclerkId",["clerkId"]),
+    }).index("by_clerk_id",["clerkId"]),
+interviews: defineTable({
+    title: v.string(),
+    description: v.optional(v.string()),
+    startTime: v.number(),
+    endTime: v.optional(v.number()),
+    status: v.string(),
+    streamCallId: v.string(),
+    candidateId: v.string(),
+    interviewerIds: v.array(v.string()),
+  })
+    .index("by_candidate_id", ["candidateId"])
+    .index("by_stream_call_id", ["streamCallId"]),
+  comments: defineTable({
+    content: v.string(),
+    rating: v.number(),
+    interviewerId: v.string(),
+    interviewId: v.id("interviews"),
+  }).index("by_interview_id", ["interviewId"]),
 });

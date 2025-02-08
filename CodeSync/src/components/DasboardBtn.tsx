7801d49e6e382a5d143a498cf1ea495201d8806c
Aman Belwal <amanbelwal11@gmail.com>
2025-02-08 14:04:46 +0530

    meeting room setup

--- CodeSync/src/components/DasboardBtn.tsx
+++ CodeSync/src/components/DasboardBtn.tsx
@@ -2,12 +2,12 @@
 import { Sparkle, SparklesIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import  Link from "next/link";
+import { useUserRole } from "@/hooks/useUserRole";
 
 
 function DasboarBtn() {
-  const isCandidate=false;
-  const isInterviewer=true;
-  if (isCandidate) return null;
+  const { isCandidate,isLoading }= useUserRole();
+  if ( isCandidate || isLoading) return null;
 
   return (<Link href={"/dashboard"}>
     <Button className="gap-2 font-medium" size={"sm"}>

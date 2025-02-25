21cc11f735e3df4d4cb906646dcf169a9a10d41e
Aman Belwal <amanbelwal11@gmail.com>
2025-02-26 01:09:03 +0530

    changed

--- 01basicreact/src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <h1>HEllo REact </h1>
-  );
-}
-
-export default App;
--- 01basicreact/src/Volley.js
+++
@@ -1,9 +0,0 @@
-function Volley () {
-    return (
-       <h2>Time to play VOlley</h2>
-    );
-}
-
-export default Volley;
-
-// Volley act as a Component here
